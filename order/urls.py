b591bd19efb3ee09fd6f6538cdeb735862da0ee9
Deepjyoti13 <deepjyotide13@gmail.com>
2021-01-05 21:08:42 +0530

    update

--- order/urls.py
+++ order/urls.py
@@ -3,8 +3,10 @@ from . import views
 urlpatterns = [
     # path('', views.index, name="Order"),
     path('addtoshopcart/<int:id>', views.addtoshopcart, name="addtoshopcart"),
+    path('addtowishlist/<int:id>', views.addtowishlist, name="addtowishlist"),
     path('deletefromcart/<int:id>', views.deletefromcart, name="deletefromcart"),
     path('shopcart/', views.shopcart, name="shopcart"),
+    path('wishlist/', views.wishlist, name="wishlist"),
     path('orderproduct/', views.orderproduct, name='payu_demo'),
     path('success', views.payu_success, name='success'),
     path('failure', views.payu_failure, name='failure'),
--- order/views.py
+++ order/views.py
@@ -230,3 +230,64 @@ def payu_failure(request):
     data = {k: v[0] for k, v in dict(request.POST).items()}
     response = payu.verify_transaction(data)
     return render(request, 'order/order_failed.html', {"ordercode": data["txnid"]})
+
+def wishlist(request):
+    category = Category.objects.all()
+    current_user = request.user  # Access User Session information
+    shopcart = WishList.objects.filter(user_id=current_user.id)
+    # shipping > ZIP 799000 to 799250 shipping = 0;
+    total = 0
+    for rs in shopcart:
+        total += rs.product.price * rs.quantity
+    # return HttpResponse(str(total))
+    context = {'shopcart': shopcart,
+               'category': category,
+               'total': total,
+               }
+    return render(request, 'eshop/wishlist.html', context)
+
+def addtowishlist(request, id):
+    url = request.META.get('HTTP_REFERER')  # get last url
+    current_user = request.user  # Access User Session information
+    product = Product.objects.get(pk=id)
+    checkinproduct = WishList.objects.filter(
+        product_id=id, user_id=current_user.id)  # Check product in shopcart
+    if checkinproduct:
+        control = 1  # The product is in the cart
+    else:
+        control = 0  # The product is not in the cart"""
+
+    if request.method == 'POST':  # if there is a post
+        form = WishListForm(request.POST)
+        if form.is_valid():
+            if control == 1:  # Update  shopcart
+                data = WishList.objects.get(
+                    product_id=id, user_id=current_user.id)
+                data.quantity += form.cleaned_data['quantity']
+                data.save()  # save data
+            else:  # Inser to Shopcart
+                data = WishList()
+                data.user_id = current_user.id
+                data.product_id = id
+                # data.variant_id = variantid
+                data.quantity = form.cleaned_data['quantity']
+                data.save()
+        else:
+            print(form.errors)
+        messages.success(request, "Product added to Wish List ")
+        return HttpResponseRedirect(url)
+
+    else:  # if there is no post
+        if control == 1:  # Update  shopcart
+            data = WishList.objects.get(product_id=id, user_id=current_user.id)
+            data.quantity += 1
+            data.save()  #
+        else:  # Inser to Shopcart
+            data = WishList()  # model ile bağlantı kur
+            data.user_id = current_user.id
+            data.product_id = id
+            data.quantity = 1
+            # data.variant_id =None
+            data.save()  #
+        messages.success(request, "Product added to Wish List")
+        return HttpResponseRedirect(url)
