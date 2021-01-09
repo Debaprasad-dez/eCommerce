e4412251ee37a2e898eb8eda5df47edcceda2a23
Deepjyoti13 <deepjyotide13@gmail.com>
2021-01-10 02:23:54 +0530

    update

--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Buckler Admin"
+admin.site.site_title = "Buckler Admin Panel"
+admin.site.index_title = "Buckler Strong Room"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('eshop.urls')),
