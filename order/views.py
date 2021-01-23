713da08da53da643e9af2fddd3d92235f51bcfc2
Deepjyoti13 <deepjyotide13@gmail.com>
2021-01-24 01:38:12 +0530

    update

--- order/views.py
+++ order/views.py
@@ -55,7 +55,8 @@ def addtoshopcart(request,id):
                 data = ShopCart()
                 data.user_id = current_user.id
                 data.product_id =id
-                data.variant_id = variantid
+                if product.variant != 'None':
+                    data.variant_id = variantid
                 data.quantity = form.cleaned_data['quantity']
                 data.save()
         messages.success(request, "Product added to Shopcart ")
